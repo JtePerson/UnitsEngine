69a77292259d3b3ae44aeee93da30ead9e415cba
JtePerson <jterlenmeyer@gmail.com>
2025-12-21 17:45:38 -0800

    2025/12/21 5:45pm
    - Engine Run/Quit

--- cmake/cpp_files.cmake
+++ cmake/cpp_files.cmake
@@ -2,4 +2,5 @@ include_guard()
 
 target_sources(UnitsEngine
 PRIVATE
+  ${UnitsEngine_SOURCE_DIR}/src/engine.cpp
 )
---
+++ src/engine.cpp
@@ -0,0 +1,7 @@
+module;
+
+module units.Engine;
+
+extern "C" namespace units {
+  Engine* Engine::s_instance_ptr_= nullptr;
+} // namespace units
